9289ed27293891394fc1c8a7efbdc90549b8a8c4
Destroyer <kasatkinilyaandreevich@gmail.com>
2021-02-13 16:17:53 +0300

    Added Homework 1

---
+++ Homework1/Homework1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+namespace Homework1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line;
+            int count = System.IO.File.ReadAllLines(args[0]).Length;
+            int[] nums = new int[count];
+            int i = 0;
+            System.IO.StreamReader file = new System.IO.StreamReader(args[0]);
+            while ((line = file.ReadLine()) != null)
+            {
+                nums[i] = int.Parse(line);
+                i++;
+            }
+            Array.Sort(nums);
+            Array.Reverse(nums);
+            Console.WriteLine(nums[0] + nums[1]);
+            file.Close();
+        }
+    }
+}
